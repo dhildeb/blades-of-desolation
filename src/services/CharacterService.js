65c664904b733e89030f223c9b39e8570aafe567
daniel hildebrandt <d.hildebrandt93@gmail.com>
2022-03-13 21:09:13 -0600

    fix

--- src/services/CharacterService.js
+++ src/services/CharacterService.js
@@ -7,18 +7,14 @@ class CharacterService{
     this.endPhase()
   }
   prepPhase(){
+    this.removeDestroyedCharacters()
     this.resetActions()
   }
   attackPhase(){
 
   }
   endPhase(){
-    $store.state.player.characters.forEach(c => {
-      if(c.hp < c.baseHp*-2){
-        $store.commit('destroyCharacter', c.id)
-        Notify.toast(c.name+' was destroyed', 'error')
-      }
-    })
+
   }
   addCharacter(character){
     $store.state.player.characters.push(character)
@@ -34,6 +30,14 @@ class CharacterService{
     let totalLvl = 0
     $store.state.player.characters.forEach(c => totalLvl += c.level)
     return Math.floor(totalLvl/$store.state.player.characters.length)
+  }
+  removeDestroyedCharacters(){
+    $store.state.player.characters.forEach(c => {
+      if(c.hp < c.baseHp*-2){
+        $store.commit('destroyCharacter', c.id)
+        Notify.toast(c.name+' was destroyed', 'error')
+      }
+    })
   }
 }
 export const characterService = new CharacterService()
