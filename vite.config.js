0d0628bde2d3fda51e2d2373b5115135dd23fe7e
daniel hildebrandt <d.hildebrandt93@gmail.com>
2022-07-24 15:26:33 -0600

    vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: '../Keepr/wwwroot',
+    sourcemap: false
+  },
+  server: {
+    port: 8080
+  }
+})
