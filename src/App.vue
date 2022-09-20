372e37fa19c9afe1f519533857f70d1e54bcca87
daniel hildebrandt <d.hildebrandt93@gmail.com>
2022-09-19 19:31:20 -0600

    test img

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
       if($store.state.characterImgList.length > 0){
         return
       }
-      let folder = (window.origin.include('localhost') ? "assets/" : "")+"img/characters/"
+      let folder = (window.origin.includes('localhost') ? "assets/" : "")+"img/characters/"
       $.ajax({
         url: folder,
         async: false,
@@ -36,7 +36,7 @@ export default {
           })
         }
       })
-      folder = (window.origin.include('localhost') ? "assets/" : "")+"img/locations/"
+      folder = (window.origin.includes('localhost') ? "assets/" : "")+"img/locations/"
       $.ajax({
         url: folder,
         async: false,
@@ -48,7 +48,7 @@ export default {
           })
         }
       })
-      folder = (window.origin.include('localhost') ? "assets/" : "")+"img/monsters/"
+      folder = (window.origin.includes('localhost') ? "assets/" : "")+"img/monsters/"
       $.ajax({
         url: folder,
         async: false,
@@ -60,7 +60,7 @@ export default {
           })
         }
       })
-      folder = (window.origin.include('localhost') ? "assets/" : "")+"img/"
+      folder = (window.origin.includes('localhost') ? "assets/" : "")+"img/"
       $.ajax({
         url: folder,
         async: false,
